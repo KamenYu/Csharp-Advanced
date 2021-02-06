724d27972ba141f165d0a176634dc365b5e15b29
Kamen Yurukov <kamen.yurukov@gmail.com>
2021-02-06 08:52:09 +0200

    Abstraction and Interfaces

---
+++ CSharp OOP/AbstractionAndIterfacesLAB/Cars/Seat.cs
@@ -0,0 +1,27 @@
+﻿namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}\nEngine start\nBreaaak!";
+        }
+    }
+}
